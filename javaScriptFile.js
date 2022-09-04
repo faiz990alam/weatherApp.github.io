5f439a5c7c1f555f99fba05ae0c154f102b45488
faiz990alam <89933146+faiz990alam@users.noreply.github.com>
2022-09-04 10:56:39 +0530

    Weather App

---
+++ javaScriptFile.js
@@ -0,0 +1,25 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=92310efd328a77254cf30900e53e871b')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Weather - "+descValue;
+  temp.innerHTML = "Temperature - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Oops! Wrong city name!"));
+})
